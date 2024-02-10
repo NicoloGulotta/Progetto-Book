49a2f580f15216bb2b58db6fc0d68c81d1ff4620
Nicolò Gulotta <Gulottanico1998@gmail.com>
2024-02-10 09:58:56 +0100

    Prova aggiunta al carrello

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
-let buttonSearch = document.getElementById("starReserch");
-let inputText = document.getElementById("input-text");
 
-buttonSearch.addEventListener("click", function() {
-  let inputTextValue = inputText.value;
-  console.log(inputTextValue);
-    const apiUrl = `https://striveschool-api.herokuapp.com/books?q=${inputTextValue}`;
-  
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        const myDiv = document.getElementById('book-box');
-        myDiv.innerHTML = '';
-  
+//creazione card prendendo dati da API 
+const bookBox = document.getElementById('book-box');
+const row = bookBox.querySelector('.row');
+
+const apiUrl = `https://striveschool-api.herokuapp.com/books`;
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
         data.forEach(book => {
-          console.log(book);
-          const bookCard =document.createElement('li');
-          bookCard.classList.add('list-group-item')
-          const bookImg = document.createElement('img');
-          bookImg.src = book.image;
-          bookCard.appendChild(bookImg);
-          myDiv.appendChild(bookCard);
-        });
-      })
-      .catch(error => console.error(error));
-  });
+            const bookItemBox = document.createElement('div');
+            bookItemBox.classList.add('book-item', 'card', 'col-md-4', 'm-1');
+            bookItemBox.style.width = '13rem';
+            // bookItemBox.style.height = 'max-content';
+
+            const bookItemImg = document.createElement('img');
+            bookItemImg.classList.add('card-img-top');
+            bookItemImg.src = book.img;
+            bookItemImg.alt = 'Book Image';
+
+            const cardBody = document.createElement('div');
+            cardBody.classList.add('card-body');
+
+            const bookTitle = document.createElement('h5');
+            bookTitle.classList.add('card-title');
+            bookTitle.textContent = book.title;
+
+            const bookPrice = document.createElement('p');
+            bookPrice.classList.add('card-text');
+            bookPrice.textContent = 'Price: ' + book.price + '$';
+
+            const bookBtnShoop = document.createElement('button');
+            bookBtnShoop.classList.add('btn', 'btn-primary');
+            bookBtnShoop.textContent = 'Aggiungi al carrello';
+
+            bookBtnShoop.addEventListener('click', function (event) {
+                const cartItem = document.createElement('li');
+                cartItem.classList.add('dropdown-item');
+                cartItem.textContent = book.title;
+                document.getElementById('cart-dropdown').appendChild(cartItem);
+            });
+
+            cardBody.appendChild(bookTitle);
+            cardBody.appendChild(bookPrice);
+            cardBody.appendChild(bookBtnShoop);
+            bookItemBox.appendChild(bookItemImg);
+            bookItemBox.appendChild(cardBody);
+            row.appendChild(bookItemBox);
+        })
+    })
+    .catch(error => console.error(error));
+
